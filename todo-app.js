4015491ef1c152ea922353be5245bab482394550
Qasim-asb <alishah.qasim4@gmail.com>
2025-03-25 02:13:43 +0500

    Added task editing feature

--- todo-app.js
+++ todo-app.js
@@ -1,25 +1,58 @@
+const todoAppContainer = document.querySelector(".todo-app-container");
 const taskInput = document.querySelector(".task-input");
 const addTaskBtn = document.querySelector(".add-task-btn");
 const taskList = document.querySelector(".task-list");
 
-addTaskBtn.addEventListener("click", () => {
-  const taskText = taskInput.value.trim();
+let currentlyEditing = null;
 
-  if (taskText !== "") {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      <span>${taskText}</span>
-      <button class="delete-task-btn">Delete</button>
-    `;
+todoAppContainer.addEventListener("click", (e) => {
+  if (e.target === addTaskBtn) {
+    const taskText = taskInput.value.trim();
 
-    taskList.appendChild(li);
+    if (taskText !== "") {
+      if (!currentlyEditing) {
+        const li = document.createElement("li");
+        li.innerHTML = `
+          <span>${taskText}</span>
+          <div>
+            <button class="edit-task-btn">Edit</button>
+            <button class="remove-task-btn">Remove</button>
+          </div>
+        `;
+        taskList.append(li);
+      } else {
+        currentlyEditing.querySelector("span").textContent = taskText;
+        currentlyEditing = null;
+        addTaskBtn.textContent = "Add Task";
+      }
+      taskInput.value = "";
+      taskInput.focus();
+    }
+  }
 
-    taskInput.value = "";
+  if (e.target.classList.contains("remove-task-btn")) {
+    e.target.closest('li').remove();
+    taskInput.focus();
+  }
 
-    const deleteBtn = li.querySelector(".delete-task-btn");
-    deleteBtn.addEventListener("click", () => {
-      taskList.removeChild(li);
-    });
+  if (e.target.classList.contains("edit-task-btn")) {
+    const li = e.target.closest("li");
+    const span = li.querySelector("span");
+
+    if (currentlyEditing === li) {
+      const taskText = taskInput.value.trim();
+      if (taskText !== "") {
+        span.textContent = taskText;
+      }
+      currentlyEditing = null;
+      addTaskBtn.textContent = "Add Task";
+      taskInput.value = "";
+    } else {
+      currentlyEditing = li;
+      taskInput.value = span.textContent;
+      addTaskBtn.textContent = "Save";
+    }
+    taskInput.focus();
   }
 });
 
